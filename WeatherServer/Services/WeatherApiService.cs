26a11db574748fa5e1df0f4b8c99ec1d1bd96aac
Ondřej Mikolášek <mikolasek.ondrej@ssakhk.cz>
2025-10-12 19:01:47 +0200

    Add project files.

---
+++ WeatherServer/Services/WeatherApiService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using OpenMeteo;
+using WeatherServer.Protos;
+
+namespace WeatherServer.Services
+{
+    public class WeatherApiService: WeatherService.WeatherServiceBase
+    {
+        private readonly OpenMeteoClient _meteoClient;
+
+        public WeatherApiService()
+        {
+            _meteoClient = new OpenMeteoClient();
+        }
+        public override Task<CurrentResponse> GetCurrent(CurrentRequest request, ServerCallContext context)
+        {
+            return base.GetCurrent(request, context);
+        }
+
+        public override Task<HourlyResponse> GetHourly(HourlyRequest request, ServerCallContext context)
+        {
+            return base.GetHourly(request, context);
+        }
+
+        public override Task<DailyResponse> GetDaily(DailyRequest request, ServerCallContext context)
+        {
+            return base.GetDaily(request, context);
+        }
+    }
+}
